404988a816fff4f4340eb829d33f739bd076c65b
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-01-02 17:48:21 +0100

    Add eslintrc for static code analysis of Javascript files

--- tests/unit/createDialog.test.js
+++ tests/unit/createDialog.test.js
@@ -1,6 +1,5 @@
 const createDialog = require("../../src/createDialog");
-console.log(createDialog);
 
-test('knowledgeTypeOptions', () => {
+test("knowledgeTypeOptions", () => {
 	expect(createDialog.knowledgeTypeOptions.length).toBe(5);
 });
